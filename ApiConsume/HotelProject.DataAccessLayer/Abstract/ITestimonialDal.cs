7d94c52ffd1235b4a088cb349edf321d5f66d2ff
zeynepdurmus <zeynep.durmus@spike.com.tr>
2025-05-27 18:53:54 +0300

    abstract interfaces

---
+++ ApiConsume/HotelProject.DataAccessLayer/Abstract/ITestimonialDal.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HotelProject.EntityLayer.Concrete;
+
+namespace HotelProject.DataAccessLayer.Abstract;
+
+interface ITestimonialDal:IGenericDal<Testimonial>
+{
+}
