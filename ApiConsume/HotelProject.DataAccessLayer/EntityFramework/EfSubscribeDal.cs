07a5f49fb5a4979b353a7b5e01428003b4be857e
zeynepdurmus <zeynep.durmus@spike.com.tr>
2025-05-27 19:29:56 +0300

    ef classes

---
+++ ApiConsume/HotelProject.DataAccessLayer/EntityFramework/EfSubscribeDal.cs
@@ -0,0 +1,14 @@
+﻿using HotelProject.DataAccessLayer.Abstract;
+using HotelProject.DataAccessLayer.Concrete;
+using HotelProject.DataAccessLayer.Repositories;
+using HotelProject.EntityLayer.Concrete;
+
+namespace HotelProject.DataAccessLayer.EntityFramework;
+
+public class EfSubscribeDal : GenericRepository<Subscribe>, ISubscribesDal
+{
+    public EfSubscribeDal(Context context) : base(context)
+    {
+
+    }
+}
